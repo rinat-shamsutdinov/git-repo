758e67a548f9343fbdfd5f8da518a9af8ef1fb68
rinat-shamsutdinov <mels_rinat@mail.ru>
2024-01-15 20:59:18 +0300

    added go_1.java and updated hello_1.java

--- hello_1.java
+++ hello_1.java
@@ -1,5 +1,5 @@
-System.out.println("Hello World!")
-System.out.println("Hello pretty!")
-System.out.println("Hey gyus!")
+System.out.println("Hello World!");
+System.out.println("Hello pretty!");
+System.out.println("Hey gyus!");
 System.out.println("Hello World!");
 System.out.println("Hello pretty!");
